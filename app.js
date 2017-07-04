617ae224d6c1b609cda2643d033b4b781ae6ec67
JonKoala <victor.santana.lemos@gmail.com>
2017-07-04 14:10:42 -0300

    adicionando appconfig.js para a solucao em angular. Quando a mesma sumir, esse arquivo deve ser removido

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-$.getJSON('appconfig.json', function(data) {
-  window.SapoApiURL = data.url.api + '/legacy/';
-  angular.module('MyTutorialApp',['treeControl']);
-});
+angular.module('MyTutorialApp',['treeControl']);
